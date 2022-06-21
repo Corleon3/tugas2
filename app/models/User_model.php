1436ed8a8006293bbf25b39ea9d64de2eb76e634
Mhd. Rian Syafrizal <96186694+Corleon3@users.noreply.github.com>
2022-06-21 17:43:18 +0700

    tugas2

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+class User_model{
+     private $nama = 'Mhd. Rian Syafrizal';
+
+     public function getUser(){
+          return $this->nama;
+     }
+}
+
+?>
---
+++ app/views/home/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+     <div class="jumbotron" style="margin-top: 50px;">
+          <h4 class="display-3">Selamat Datang <b><?php echo $data["nama"]; ?></b> di halaman Home</h4>
+          <img src="<?php echo BASEURL; ?>/img/4.jpg" alt="img" style="width: 50%;">
+          <p class="lead">Hai Rian, Selamat datang , NIM saya <?php echo $data["nim"]; ?>, saya dari Jurusan <?php echo $data["jurusan"]; ?></p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+     </div>
+</div>
